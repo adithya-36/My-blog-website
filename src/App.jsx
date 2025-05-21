03aefb5de22176adbc2b2f7a5e0d092e820a1f54
Adithya Shaji <adithyas038@gmail.com>
2025-05-22 00:18:08 +0530

    Added routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,43 @@
 import React from 'react'
 import Navbar from './components/Navbar'
 import Home from './components/Home'
+import About from './components/About'
 import Blog from './components/Blog'
 import Contact from './components/Contact'
+import AboutPage from './pages/AboutPage'
+import BlogPage from './pages/BlogPage'
+import ContactPage from './pages/ContactPage' 
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 
 const App = () => {
+  const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <>
+    <Navbar/>
+    <Home/>
+    <About/>
+    <Blog/>
+    <Contact/>
+    </>
+  },
+  {
+    path: "/about",
+    element: <><Navbar/><AboutPage/></>
+  },
+  {
+    path: "/blog",
+    element: <><Navbar/><BlogPage/></>
+  },
+  {
+    path: "/contact",
+    element: <><Navbar/><ContactPage/></>
+  }
+  ])
   return (
     <div >
-      <Navbar/>
-      <Home/>
-      <Blog/>
-      <Contact/>
+      
+      <RouterProvider router={router} />
     </div>
   )
 }
